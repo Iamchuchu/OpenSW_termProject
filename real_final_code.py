7deb59f517911ea7f710715f3f11d95ba86c9176
Betty Kim <kda5337031112@gmail.com>
2023-12-26 13:38:34 +0900

    final code commit1

--- real_final_code.py
+++ real_final_code.py
@@ -1,66 +1,49 @@
 import cv2
 import numpy as np
-import cvlib as cv
-from cvlib.object_detection import draw_bbox
+from cvlib.object_detection import detect_common_objects, draw_bbox
 from keras.models import load_model
 from statistics import mode
 from utils.datasets import get_labels
-from utils.inference import draw_text
-from utils.inference import draw_bounding_box
-from utils.inference import apply_offsets
+from utils.inference import draw_text, draw_bounding_box, apply_offsets
 from utils.preprocessor import preprocess_input
 
-USE_WEBCAM = True # If false, loads video file source
-
-# parameters for loading data and images
+# Load the emotion model and labels
 emotion_model_path = './models/emotion_model.hdf5'
 emotion_labels = get_labels('fer2013')
+emotion_classifier = load_model(emotion_model_path)
+emotion_target_size = emotion_classifier.input_shape[1:3]
+
+# Load the face cascade classifier
+face_cascade = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
 
 # hyper-parameters for bounding boxes shape
 frame_window = 10
 emotion_offsets = (20, 40)
 
-# loading models
-face_cascade = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
-emotion_classifier = load_model(emotion_model_path)
-
-# getting input model shapes for inference
-emotion_target_size = emotion_classifier.input_shape[1:3]
-
 # starting lists for calculating modes
 emotion_window = []
 
-# starting video streaming
 
-cv2.namedWindow('window_frame')
-video_capture = cv2.VideoCapture(0)
-
-# Select video or webcam feed
-cap = None
-if (USE_WEBCAM == True):
-    cap = cv2.VideoCapture(0) # Webcam source
-else:
-    cap = cv2.VideoCapture('./demo/dinner.mp4') # Video file source
-
-while cap.isOpened(): # True:
-    ret, bgr_image = cap.read()
-
+def process_image(bgr_image):
     gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-    rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
 
-    # apply object detection (물체 검출)
-    bbox, label, conf = cv.detect_common_objects(bgr_image)
-    print(bbox, label, conf)
+    # Detect faces using the face cascade classifier
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
+                                          flags=cv2.CASCADE_SCALE_IMAGE)
 
-    # draw bounding box over detected objects
-    out = draw_bbox(bgr_image.copy(), bbox, label, conf, write_conf=True)
+    # Initialize output image
+    out = bgr_image.copy()
 
-    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5,minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    # Detect common objects
+    bbox, label, conf = detect_common_objects(bgr_image)
+
+    # Draw bounding box over detected objects
+    out = draw_bbox(out, bbox, label, conf, write_conf=True)
 
     for face_coordinates in faces:
-
         x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
         gray_face = gray_image[y1:y2, x1:x2]
+
         try:
             gray_face = cv2.resize(gray_face, (emotion_target_size))
         except:
@@ -69,6 +52,7 @@ while cap.isOpened(): # True:
         gray_face = preprocess_input(gray_face, True)
         gray_face = np.expand_dims(gray_face, 0)
         gray_face = np.expand_dims(gray_face, -1)
+
         emotion_prediction = emotion_classifier.predict(gray_face)
         emotion_probability = np.max(emotion_prediction)
         emotion_label_arg = np.argmax(emotion_prediction)
@@ -77,11 +61,14 @@ while cap.isOpened(): # True:
 
         if len(emotion_window) > frame_window:
             emotion_window.pop(0)
+
         try:
             emotion_mode = mode(emotion_window)
         except:
             continue
 
+        # Color mapping based on emotion
+        color = None
         if emotion_text == 'angry':
             color = emotion_probability * np.asarray((255, 0, 0))
         elif emotion_text == 'sad':
@@ -96,18 +83,21 @@ while cap.isOpened(): # True:
         color = color.astype(int)
         color = color.tolist()
 
+        # Draw bounding box only when a person is detected in the image
         draw_bounding_box(face_coordinates, out, color)
-        draw_text(face_coordinates, out, emotion_mode,
-                  color, 0, -45, 1, 1)
+        draw_text(face_coordinates, out, emotion_mode, color, 0, -45, 1, 1)
 
-    bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+    # Display the result
+    cv2.imshow('Output', out)
+    cv2.waitKey(0)
 
-    # Combine frames
-    combined_frame = np.hstack((out, bgr_image))
 
-    cv2.imshow('Combined Frame', combined_frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# Use the provided image
+image_path = './part 2/women_book.jpg'
 
-cap.release()
-cv2.destroyAllWindows()
+if image_path:
+    # Load the image
+    bgr_image = cv2.imread(image_path)
+    process_image(bgr_image)
+else:
+    print("Error: Image path not provided.")
